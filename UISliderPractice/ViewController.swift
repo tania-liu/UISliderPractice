b31e403de97aaf6cca580f44da968afb32d969bf
Tania Liu <tanialiu430006@gmail.com>
2023-10-19 16:13:54 +0800

    Initial Commit

---
+++ UISliderPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISliderPractice
+//
+//  Created by 劉俐廷 on 2023/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
